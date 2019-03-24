057a76cebe075ad997d5020e2503ca4256179664
mosca <alitomosca@gmail.com>
2019-03-24 12:08:04 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+
+// Init express app & server
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Starts the server.
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+
+// Add the WebSocket handlers
+io.on('connection', function(socket: any) {
+});
+
+const players: any = {};
+io.on('connection', function(socket: any) {
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  socket.on('movement', function(data: any) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+});
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
---
+++ src/static/game.ts
@@ -0,0 +1,62 @@
+var socket = io();
+
+var movement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+}
+document.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+        case 65: // A
+        movement.left = true;
+        break;
+        case 87: // W
+        movement.up = true;
+        break;
+        case 68: // D
+        movement.right = true;
+        break;
+        case 83: // S
+        movement.down = true;
+        break;
+    }
+});
+document.addEventListener('keyup', function(event) {
+switch (event.keyCode) {
+    case 65: // A
+    movement.left = false;
+    break;
+    case 87: // W
+    movement.up = false;
+    break;
+    case 68: // D
+    movement.right = false;
+    break;
+    case 83: // S
+    movement.down = false;
+    break;
+}
+});
+
+socket.emit('new player');
+setInterval(function() {
+  socket.emit('movement', movement);
+}, 1000 / 60);
+
+const canvas = <HTMLCanvasElement> document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+const context = canvas.getContext('2d');
+
+const playerImg = new Image();
+playerImg.src = "https://opengameart.org/sites/default/files/DungeonCrawl_ProjectUtumnoTileset.png";
+
+socket.on('state', function(players: any) {
+  context.clearRect(0, 0, 800, 600);
+  context.fillStyle = 'green';
+  for (var id in players) {
+    var player = players[id];
+    context.drawImage(playerImg, 0, 64, 32, 32, player.x, player.y, 32, 32);
+  }
+});
