2850a7a279b8b5d8c498504e3cc749cf06badb7f
mosca <alitomosca@gmail.com>
2019-04-06 17:30:17 -0300

    Larger test map

--- src/server.ts
+++ src/server.ts
@@ -180,7 +180,7 @@ server.listen(5000, function() {
 
 const players: any = {};
 const testMap: Map = new Map();
-testMap.loadFromFile("test.json");
+testMap.loadFromFile("towne.json");
 function updatePlayerChar(socketID: any, character: any): void {
   if (players[socketID]) {
     players[socketID] = character.getClientDict();
